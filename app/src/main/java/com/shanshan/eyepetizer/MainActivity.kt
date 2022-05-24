c5fc1ac0c9d8f45e7f09c70ac4f6054138a896e1
x21223_朱世鹏 <zhushipeng X21223/autel@autel>
2022-05-24 22:01:43 +0800

    commit:添加部分资源文件

--- app/src/main/java/com/shanshan/eyepetizer/MainActivity.kt
+++ app/src/main/java/com/shanshan/eyepetizer/MainActivity.kt
@@ -2,10 +2,19 @@ package com.shanshan.eyepetizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.pixplicity.sharp.Sharp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img = findViewById<ImageView>(R.id.loadsvg)
+        Sharp.loadPath("http://img.svgfont.com/2021/11/13/svgfont_83de5fdb2720211113104828416.png?x-oss-process=style/large")
+
+        /*Glide.with(this).load("http://img.svgfont.com/2021/11/13/svgfont_83de5fdb2720211113104828416.png?x-oss-process=style/large")
+            .into(img)*/
     }
 }
---
+++ app/src/main/java/com/shanshan/eyepetizer/model/AboutViewModel.java
@@ -0,0 +1,7 @@
+package com.shanshan.eyepetizer.model;
+
+import androidx.lifecycle.ViewModel;
+
+public class AboutViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
