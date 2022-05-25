b08327b75445f14489716f5d1ad959e80f916361
山杉 <1455030401@qq.com>
2022-05-26 00:13:27 +0800

    commit：navigation完成首页切换

--- app/src/main/java/com/shanshan/eyepetizer/MainActivity.kt
+++ app/src/main/java/com/shanshan/eyepetizer/MainActivity.kt
@@ -3,18 +3,32 @@ package com.shanshan.eyepetizer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.bumptech.glide.Glide
 import com.pixplicity.sharp.Sharp
+import com.shanshan.eyepetizer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val img = findViewById<ImageView>(R.id.loadsvg)
-        Sharp.loadPath("http://img.svgfont.com/2021/11/13/svgfont_83de5fdb2720211113104828416.png?x-oss-process=style/large")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        /*Glide.with(this).load("http://img.svgfont.com/2021/11/13/svgfont_83de5fdb2720211113104828416.png?x-oss-process=style/large")
-            .into(img)*/
+        initView()
+        initData()
+    }
+
+    private fun initView() {
+        val controller =  Navigation.findNavController(this, R.id.my_nav_host_fragment)
+        NavigationUI.setupWithNavController(binding.bottomNavigation,controller)
+    }
+
+    private fun initData() {
+
     }
 }
